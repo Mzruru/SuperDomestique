41f84f7dbc749547be091b12c21b88dee497f069
Owen Bennett <owen@steamshift.com>
2014-08-11 01:02:36 +0100

    Added min and max zoom positions to camera zoom script.

--- Assets/Scripts/ZoomCameraWithSpeed.cs
+++ Assets/Scripts/ZoomCameraWithSpeed.cs
@@ -1,18 +1,20 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+// Causes the camera to follow the subject, and zoom in and out based on the subject's speed
 public class ZoomCameraWithSpeed : MonoBehaviour
 {
-
-	public GameObject subject;
-	public float minSpeed = 1f;
-	public float maxSpeed = 10f;
-	public Vector3 ease = new Vector3(0.5f, 0.5f, 0.5f);
-	public Vector3 zoomToSpeedRatio = new Vector3(1, 1, 1);
-	public int smoothOverFrames = 30;
+	[Tooltip ("What the camera should follow")] public GameObject subject;
+	[Tooltip ("The minimum speed to be used to affect the camera")] public float minSpeed = 1f;
+	[Tooltip ("The maximum speed to be used to affect the camera")] public float maxSpeed = 10f;
+	[Tooltip ("Strength of easing to the new target position. A value of 1 will snap to the target.")] public Vector3 ease = new Vector3(0.5f, 0.75f, 0.5f);
+	[Tooltip ("When the subject is at the maximum speed, the camera will be at this position.")] public Vector3 fullZoomPosition = new Vector3(1, 1, 1);
+	[Tooltip ("The number of frames over which subject speed will be smoothed. Higher value results in smoother movement.")]public int smoothOverFrames = 100;
 	
 	Vector3 subjectLastPosition;
 	Vector3 cameraOffset;
+	Vector3 minZoomOffset;
+	Vector3 maxZoomOffset;
 	Vector3 targetPosition;
 	Vector3 targetOffset;
 	
@@ -26,6 +28,8 @@ public class ZoomCameraWithSpeed : MonoBehaviour
 		subjectLastPosition = subject.transform.position;
 		cameraOffset = transform.position - subject.transform.position;
 		smoothSpeeds = new float[smoothOverFrames];
+		minZoomOffset = transform.position - subject.transform.position;
+		maxZoomOffset = fullZoomPosition - subject.transform.position;
 	 }
 	
 	// Update is called once per frame
@@ -50,8 +54,7 @@ public class ZoomCameraWithSpeed : MonoBehaviour
 		c++;
 		if (c >= smoothOverFrames) c = 0;
 		
-		targetOffset = Vector3.Scale(cameraOffset, zoomToSpeedRatio * (smoothedSpeed / minSpeed));
-			
+		targetOffset = Vector3.Lerp(minZoomOffset, maxZoomOffset, Mathf.InverseLerp(minSpeed, maxSpeed, smoothedSpeed));
 		targetPosition = subject.transform.position + targetOffset;
 		Vector3 offset = (targetPosition - transform.position);
 		offset.x *= ease.x;
