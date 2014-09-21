bf4f1552308b3fc5a353e37d7739be47f9998fd9
Owen Bennett <owen@steamshift.com>
2014-09-21 22:48:02 +0100

    Added jumping, and simplified sideways movement.

--- Assets/Scripts/SD/AutoRider.cs
+++ Assets/Scripts/SD/AutoRider.cs
@@ -13,21 +13,34 @@ public class AutoRider : MonoBehaviour {
 	[Tooltip("How much power boost using a sprint provides")]public float sprintBoostPower = 0.1f;
 	[Tooltip("How long a sprint lasts")]public float sprintDuration = 2f;
 	[Tooltip("How quickly sprint boost reduces after releasing finished")]public float sprintBoostFalloff = 0.99f;
-	[Tooltip("How heavy the bike is. Effects acceleration on hills")]public float bikeWeight = 1;
+	[Tooltip("How heavy the bike is. Effects acceleration on hills and height of jumps")]public float bikeWeight = 1;
 	[Tooltip("How fast the bike moves sideways")]public float sidewaysSpeed = 3;
+	[Tooltip("How much power jumps have")]public float jumpPower = 1;
+	[Tooltip("The strength of gravity, when jumping")]public float gravity = 8.0f;
 	
 	Bounds riderBounds;
-	float currentSpeed = 0.0f;
-	float currentSidewaysSpeed = 0.0f;
-	Vector3 normal = Vector3.zero;
 	int blockOffset = 0;
 	int currentBlockPosition;
 	GameObject currentBlock;
 	HillGenerator generator;
+	
+	// movement
+	Vector3 normal = Vector3.zero;
+	float currentSpeed = 0.0f;
+	Vector3 moveVec;
+	
+	// sprint
 	bool sprintBoosted;
 	float currentSprintBoost = 0.0f;
 	float sprintTime;
-	Vector3 moveVec;
+	
+	// dodge
+	int targetZ;
+	
+	// jump
+	bool useJump = true;
+	float jumpVelocity;
+	bool isJumping;
 	
 	// Use this for initialization
 	void Start () {
@@ -35,6 +48,7 @@ public class AutoRider : MonoBehaviour {
 		blockOffset = Mathf.FloorToInt(gameObject.transform.position.x);
 		
 		riderBounds = meshObject.GetComponent<MeshFilter>().mesh.bounds;
+		targetZ = (int)gameObject.transform.position.z;
 	}
 	
 	// Update is called once per frame
@@ -43,6 +57,8 @@ public class AutoRider : MonoBehaviour {
 		float moveX = -riderSpeed;
 		bool sprinting = Input.GetButton("Right") && !sprintBoosted && numberOfSprints > 0;
 		float accelerationOnSlope = (accelerationDueToGravity * -normal.x) * bikeWeight;
+		
+		CheckForInput();
 		
 		if (sprinting)
 		{
@@ -65,6 +81,30 @@ public class AutoRider : MonoBehaviour {
 		float currentPositionInBlock = (-generator.offset % 1);
 		UpdateCurrentBlock(newBlockPosition);
 		UpdateRiderPosition(currentBlock, currentPositionInBlock);
+	}
+	
+	void CheckForInput () {
+		if (Input.touchCount > 0 || Input.GetMouseButtonDown(0))
+		{
+			if (Input.GetMouseButtonDown(0) || Input.GetTouch(0).phase == TouchPhase.Began)
+			{
+				if (useJump) DoJump();
+				else DoDodge();	
+			}
+		}
+	}
+	
+	void DoDodge () {
+		float zPosition = gameObject.transform.position.z;
+		if (zPosition > 0) SetRiderPosition(-3);
+		else SetRiderPosition(3);
+	}
+	
+	void DoJump () {
+		if (isJumping) return;
+		isJumping = true;
+		
+		jumpVelocity = jumpPower / bikeWeight;
 	}
 	
 	void updateBasedOnSpeed (float move, float accelerationOnSlope) {
@@ -91,9 +131,20 @@ public class AutoRider : MonoBehaviour {
 	
 	void UpdateRiderPosition (GameObject block, float currentPositionInBlock) {
 		IMeshModder finder  = (IMeshModder)block.GetComponent(typeof(IMeshModder));
+		
 		moveVec = finder.GetPointOnTopFace(currentPositionInBlock, 0.5f);
 		moveVec += block.transform.position;
 		moveVec += normal * riderBounds.extents.y * meshObject.transform.localScale.y;
+		if (isJumping) {
+			Vector3 currentPosition = gameObject.transform.position;
+			currentPosition.y += jumpVelocity;
+			jumpVelocity -= gravity * Time.smoothDeltaTime;
+			if (currentPosition.y < moveVec.y) {
+				isJumping = false;
+			} else {
+				moveVec.y = currentPosition.y;
+			}
+		}
 		
 		UpdateRiderZPosition();
 		
@@ -101,25 +152,40 @@ public class AutoRider : MonoBehaviour {
 	}
 	
 	void UpdateRiderZPosition () {
+		
 		// across road
 		Vector3 position = gameObject.transform.position;
-		float newSpeed = Input.GetAxis("Vertical") * sidewaysSpeed * Time.smoothDeltaTime;
-		float newPosition = position.z - newSpeed;
-		int roadHalfWidth = ((int)generator.roadWidth / 2) - 1;
-		if (newPosition < -roadHalfWidth || newPosition > roadHalfWidth) {
-			newSpeed = 0;
-			currentSidewaysSpeed *= 0.9f;
-		} else {
-			currentSidewaysSpeed = newSpeed;
+		int input = 0;
+		if (position.z > targetZ) {
+			input = -1;
+		} else if (position.z < targetZ){
+			input = 1;
 		}
-		
-		moveVec.z = transform.position.z + newSpeed;
+			
+		float newSpeed = input * sidewaysSpeed * Time.smoothDeltaTime;
+		if (newSpeed == 0) moveVec.z = targetZ;
+		else moveVec.z = transform.position.z + newSpeed;
+	}
+	
+	// rider position is from 4 (left hand edge) to -4 (right hand edge)
+	void SetRiderPosition (int index) {
+		if (index > 4 || index < -4) return;
+		targetZ = index;
 	}
 	
 	void UpdateRiderRotation (GameObject block) {
 		IMeshModder modder = (IMeshModder)block.GetComponent(typeof(IMeshModder));
-		normal = modder.GetAverageOfTopNormal();
-		gameObject.transform.rotation = Quaternion.FromToRotation(Vector3.up, normal);
-		gameObject.transform.Rotate(new Vector3(0, 90, 0));
+		
+		if (isJumping) {
+			float rotation = -jumpVelocity * 2;
+			if (rotation < -2) rotation = -2;
+			else if (rotation > 2) rotation = 2;
+			gameObject.transform.Rotate(new Vector3(rotation, 0, 0));
+			// TODO: when the bike's rotation is too high in comparision to the normal value, make it crash
+		} else {
+			normal = modder.GetAverageOfTopNormal();
+			gameObject.transform.rotation = Quaternion.FromToRotation(Vector3.up, normal);
+			gameObject.transform.Rotate(new Vector3(0, 90, 0));
+		}
 	}
 }
